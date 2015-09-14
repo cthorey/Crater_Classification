2396b89f3037908f1558cbe40da95753091c8a85
cthorey <clement.thorey@gmail.com>
2015-09-14 15:01:35 +0200

    Tien compte ds differetne platform

--- LROC_Feature.py
+++ LROC_Feature.py
@@ -4,7 +4,25 @@ import numpy as np
 from Data_utils import *
 from Extract_Array import *
 
-racine = '/Users/thorey/Documents/These/Projet/FFC/Classification/'
+from sys import platform as _platform
+import distutils.util
+import shutil
+import datetime
+
+    
+##############################
+# Platform
+
+platform = 'laptop'
+
+if _platform == "linux" or _platform == "linux2":
+    racine = '/gpfs/users/thorey/Classification/'
+elif _platform == "darwin":
+    if platform == 'clavius':
+        racine = '/Users/clement/Classification/'
+    else:
+        racine = '/Users/thorey/Documents/These/Projet/FFC/Classification/'
+
 data_path = os.path.join(root,'Data')
 extraction_path = os.path.join(racine,'Data','ImageExtractionLROC')
 
@@ -15,13 +33,27 @@ df = pd.DataFrame(np.hstack((data.Index,data.Lat,data.Long,data.Diameter)),
 
 df_LROC = 0 # Contient les pixel 
 df_LROC_Error = []
+
+# compteur
+compteur_init = len(df)
+compteur =  compteur_init
+tracker = open('tracker_feature.txt','wr+')
+tracker.write('Debut du feature extraction \n')
+tracker.close()
+
 for i,row in df.iterrows():
     C = Crater(str(int(row.Index)),'i')
     try:
         fig = C.plot_LROC()
     except:
         df_LROC_Error.append(row)
+        tracker = open('tracker_feature.txt','a')
+        tracker.write('Le crater %d a bugger'%(int(row.Index)))
+        tracker.write('Il rest encore %d crater'%(compteur))
+        tracker.close()
+        comtpeur -= 1
         continue
+        
     Name = 'C_'+str(row.Index)+'.jpg'
     fig.savefig(os.path.join(extraction_path,Name),
                 rasterized=True,
@@ -36,7 +68,12 @@ for i,row in df.iterrows():
         df_LROC = I2
     else:
         df_LROC = np.vstack((df_LROC,I2))
-        
+
+    tracker = open('tracker_feature.txt','a')
+    tracker.write('Il rest encore %d crater'%(compteur))
+    tracker.close()
+    comtpeur -= 1
+
 data['feat_LROC'] = df_LROC
 data['feat_LROC_Error'] = df_LROC_Error
 with open(Output+'LOLA'+str(pix)+'_GRAIL_Dataset_3', 'wb') as fi:
